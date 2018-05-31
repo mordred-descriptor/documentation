10aef9ca8d66167a1f064ab6f4910560a8e77c4e
philopon <philopon.dependence@gmail.com>
2018-05-31 07:30:18 +0000

    update documentation to mordred-descriptor/mordred@17d9e97da585e1352f2d7dce78040da1b6c56270

--- develop/_static/bizstyle.js
+++ develop/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("mordred 1.1.0.post1.dev1.post1.dev1 documentation");
+        $("li.nav-item-0 a").text("mordred 1.1.2a1 documentation");
     }
 });
